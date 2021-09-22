dd0d104d50209e707bb58325d01380c8a2d60b19
unknown <alanalexisaguilar@gmail.com>
2021-09-21 23:05:22 -0500

    algoritmo de internet

--- algoritmo de euclides.py
+++ algoritmo de euclides.py
@@ -1,8 +1,29 @@
-palabras=[2750]
-print("valores ")
-for desen in palabras:
-
-    print(desen%27)
-print("lista ")
-for desen in palabras:
-    print(desen)
+def xgcd(a, b):
+    """Calcula el greater common divisor (gcd)/ máximo común divisor de a y b.
+        Utiliza el algoritmo extendido de Euclides.
+    Args:
+        a: mcd(a,b)=resultado
+        b: mcd(a,b)=resultado
+    Returns:
+        Returns mcd(a,b) and (u0,v0)
+    """
+    if b == 0:
+        return 0,1,0
+    u0 = 1
+    u1 = 0
+    v0 = 0
+    v1 = 1
+    while b != 0:
+        q = a//b
+        r = a - b * q
+        u = u0 - q * u1
+        v = v0 - q * v1
+        #Update a,b
+        a = b
+        b = r
+        #Update for next iteration
+        u0 = u1
+        u1 = u
+        v0 = v1
+        v1 = v
+    return  a, u0, v0
